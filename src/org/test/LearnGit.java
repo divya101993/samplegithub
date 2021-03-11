8a697e70a7eb6713f409025f48f6270f3c88209c
divya101993 <divyamca0793@gmail.com>
2021-03-11 12:18:41 +0530

    updated code

--- src/org/test/LearnGit.java
+++ src/org/test/LearnGit.java
@@ -1,14 +1,20 @@
 package org.test;
 public class LearnGit 
 {
+	int c;
 	public void add(int a,int b) 
 	{
-		int c;
 		c=a+b;
+		
 		System.out.println("The sum of two numbers:"+c);
+	}
+	public void mulitple(int a,int b) {
+		c=a*b;
+		System.out.println("Multiplication of two numbers:"+c);
 	}
 	public static void main(String[] args) {
 		LearnGit obj = new LearnGit();
 		obj.add(20, 50);
+		obj.mulitple(20, 10);
 		}
 }
